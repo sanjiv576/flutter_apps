06ed529e6d1a638e8e82f8e7594bdeeb2c01f351
Sanjiv Shrestha <sanjiv576>
2023-04-24 13:20:40 +0545

    add mul and divide functions

--- flutter_app/lib/model/arithmetic.dart
+++ flutter_app/lib/model/arithmetic.dart
@@ -1,8 +1,13 @@
 class Arithmetic {
-
   double add({double firstNumber = 0.0, double secondNumber = 0.0}) =>
       firstNumber + secondNumber;
 
   double subtract({double firstNumber = 0.0, double secondNumber = 0.0}) =>
       firstNumber - secondNumber;
+
+  double multiply({double firstNumber = 0, double secondNumber = 0}) =>
+      firstNumber * secondNumber;
+    
+   double divide({double firstNumber = 1, double secondNumber = 1}) =>
+      firstNumber / secondNumber;
 }
