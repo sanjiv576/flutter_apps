82220e5b488193bfb2349c0aaaa586dfadde5aa5
Sanjiv Shrestha <sanjiv576>
2023-04-24 13:22:03 +0545

    add dashoard buttons

---
+++ flutter_app/lib/view/dashboard_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/simpleInterestRoute');
+                  },
+                  child: const Text('Simple Interest')),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/arithmeticRoute');
+                  },
+                  child: const Text('Arithmetic Calculation')),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/circleRoute');
+                  },
+                  child: const Text('Area of Circle')),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/changeNameRoute');
+                  },
+                  child: const Text('Change Name')),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
