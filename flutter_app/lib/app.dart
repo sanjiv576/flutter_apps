94c366d15f53413e1b1181d2da24ccbe199f3450
Sanjiv Shrestha <sanjiv576>
2023-04-24 13:20:05 +0545

    add routes

--- flutter_app/lib/app.dart
+++ flutter_app/lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'view/arithmetic_view.dart';
+import 'view/circle_view.dart';
+import 'view/dashboard_view.dart';
+import 'view/change_name_view.dart';
 import 'view/simple_interest_view.dart';
 
 class App extends StatelessWidget {
@@ -10,7 +13,14 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SimpleInterestView(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const DashboardView(),
+        '/arithmeticRoute': (context) => const ArithmeticView(),
+        '/simpleInterestRoute': (context) => const SimpleInterestView(),
+        '/circleRoute': (context) => const CircleView(),
+        '/changeNameRoute': (context) => const ChangeNameView()
+      },
     );
   }
 }
