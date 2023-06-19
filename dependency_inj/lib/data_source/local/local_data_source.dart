907e56e607e76f629c69ad79ac4e8f340b0613ba
Sanjiv Shrestha <sanjiv576>
2023-06-19 20:41:40 +0545

    update

--- dependency_inj/lib/data_source/local/local_data_source.dart
+++ dependency_inj/lib/data_source/local/local_data_source.dart
@@ -5,7 +5,7 @@ class LocalDataSource {
   List<User> users = [
     User(id: 1, name: 'Local John', dob: '1/6/2001', birthTime: '14:22'),
     User(id: 2, name: 'Local Jane', dob: '12/12/2014', birthTime: '12:22'),
-    User(id: 3, name: 'Sanjiv Shrestha', dob: '15/6/2016', birthTime: '10:30'),
+    // User(id: 3, name: 'Sanjiv Shrestha', dob: '15/6/2016', birthTime: '10:30'),
   ];
 
 // add user
