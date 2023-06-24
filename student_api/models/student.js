6d9362ccab28469342d406e2a825a16d2b6e2832
Sanjiv Shrestha <sanjiv576>
2023-06-24 07:40:33 +0545

    update

--- student_api/models/student.js
+++ student_api/models/student.js
@@ -49,10 +49,11 @@ studentSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Sign JWT and return
+// Sign JWT and return   ===> token generation
 studentSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
+    // expiresIn: 5,  // expires in 5 sec
   });
 };
 
